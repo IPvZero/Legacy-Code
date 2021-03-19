529f6efaf3a8c08998dd68f413017265059e4d99
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-03-19 19:59:38 +0000

    Update showipintbri.py

--- showipintbri.py
+++ showipintbri.py
@@ -3,5 +3,5 @@ from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
 
 nr = InitNornir(config_file="config.yaml")
-result = nr.run(task=netmiko_send_command, command="show ip interface brief")
+result = nr.run(task=netmiko_send_command, command_string="show ip interface brief")
 print_result(result)
